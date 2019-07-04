a4e642ac42e473fd5df2d2ab8364c8400b3107fe
Aryane Silva <aryane.silva@mv.com.br>
2019-07-04 17:48:02 -0300

    Desenvolvimento do tema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Question } from './question';
 import { QuestionGateway } from './question.gateway';
+import { FormControl } from '@angular/forms';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  theme = false;
   questions: Question[];
+
+  themeControl = new FormControl('light-blue');
+
+  linkRel = document.querySelector('#themeControl') as HTMLLinkElement;
   constructor(
     private readonly gateway: QuestionGateway
   ) {
     this.questions = this.gateway.getQuestions();
+  }
+
+  ngOnInit() {
+    (this.themeControl.valueChanges as Observable<string>)
+    .pipe(
+      map(theme => `./themes/${theme}.css`)
+    ).subscribe(sourceUrl => {
+      console.log(sourceUrl);
+      this.linkRel.href = sourceUrl;
+    });
+  }
+
+  isAlternateMode() {
+    this.theme = !this.theme;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { QuestionControlGateway } from './question-control.gateway';
 import { DynamicFormComponent } from './dynamic-form.component';
 import { DynamicFormQuestionComponent } from './dynamic-form-question.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { QuestionGateway } from './question.gateway';
+import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { CommonModule } from '@angular/common';
@@ -24,12 +26,16 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     CommonModule,
     BrowserModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
     FormsModule,
     MatInputModule,
     MatButtonModule,
     MatSelectModule,
     MatExpansionModule,
     NoopAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSelectModule,
   ],
   providers: [
     QuestionControlGateway,
